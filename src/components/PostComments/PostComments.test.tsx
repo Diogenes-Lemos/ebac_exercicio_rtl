b33b716eb2e231a3b9613d0afbd587f7d47a8172
Diogenes-Lemos <diogenes.lemos1@gmail.com>
2025-03-01 12:32:21 -0300

    Adicionado o segundo teste

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,23 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
+import { render, screen, fireEvent } from '@testing-library/react';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+    it('Deve testar a inserção de dois comentários', () => {
+        render(<PostComment/>);
+
+        const textarea = screen.getByRole('textbox');
+        const button = screen.getByText('Comentar');
+
+        fireEvent.change(textarea, { target: { value: 'Primeiro comentário' } });
+        fireEvent.click(button);
+
+        fireEvent.change(textarea, { target: { value: 'Segundo comentário' } });
+        fireEvent.click(button);
+
+        expect(screen.getAllByTestId('comentario')).toHaveLength(2);
     });
 });
